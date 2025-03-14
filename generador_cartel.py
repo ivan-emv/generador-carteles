917aee48d28d886b3f95c35f11b757f2994256ef
ivan-emv <ivan.amador@europamundo.com>
2025-03-14 19:52:50 +0100

    Update generador_cartel.py

--- generador_cartel.py
+++ generador_cartel.py
@@ -62,7 +62,7 @@ def generar_cartel(ciudad, fecha, actividad, hora_encuentro, punto_encuentro, de
                 run.font.color.rgb = RGBColor(44, 66, 148)
                 run.bold = True
             if opcionales_texto:
-                opcional_paragraph = p.insert_paragraph_after(opcionales_texto)
+                opcional_paragraph = doc.paragraphs.insert(doc.paragraphs.index(p) + 1, doc.add_paragraph(opcionales_texto))
                 opcional_paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
                 opcional_run = opcional_paragraph.add_run()
                 opcional_run.font.name = "Neulis Sans"
